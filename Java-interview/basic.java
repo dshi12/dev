a980d7b8dc09643b5686a5e269d5d9e9b9d92290
dshi12 <48572945+dshi12@users.noreply.github.com>
2022-04-07 19:07:26 -0500

    v-1

---
+++ Java-interview/basic.java
@@ -0,0 +1,19 @@
+/**
+1. JDK, JRE, JVM:
+    JDK: JRE + other tools(etc. compiler: convert java codes to byte codes)
+    JRE: JVM + class libraries + other supporting files
+    JVM: provide platform-indedepend way of executing source codes; it converts the byte codes to machine codes
+
+2. How does Method overloading works
+    Method of a class can be overloaded by:
+        a. Changing the number of parameters
+        b. Changing the type of parameters passed into method
+
+3. Can the main method be overloaded
+    yes, but only public static void main(String[] args) will be launched by JVM
+ */
+
+ /**
+ 4. What are static initialization blocks
+    A static initialization block is a normal block of code enclosed by curly braces {}, and preceded by the static keyword
+  */
